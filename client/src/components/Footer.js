28aa00339b999ddf720c9a1ec79d5772ee7efaf6
luisglezbrown <lfernanglez@gmail.com>
2021-05-26 00:35:38 +0200

    Creo componente "Footer"

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,17 @@
 import './style/Footer.css'
 
+import logo from '../images/logo.png'
+
 export default function Footer() {
     return (
-        <div className='footer'>
-            FOOTER
-        </div>
+        <footer>
+            <div className="left-container">
+                <p>Hecho con 🤍 por <a href="https://github.com/luisglezbrown" target="_blank" rel="noreferrer">Luis González</a> || Málaga [ES]</p>
+                <p>Todas las imágenes creadas por <a href="https://www.freepik.es/" target="_blank" rel="noreferrer">Freepik</a> </p>
+            </div>
+            <div className="right-container">
+                <img src={logo} alt="logo" className="logo"/>
+            </div>
+        </footer>
     )
 }
