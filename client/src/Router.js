af1d2224d7f0f28a35d32cd549ac9d2d1fe3c465
luisglezbrown <lfernanglez@gmail.com>
2021-05-29 14:08:04 +0200

    Modifico rutas para utilizar useParams

--- client/src/Router.js
+++ client/src/Router.js
@@ -5,17 +5,20 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import GuideRegister from './pages/GuideRegister';
 import AllCities from "./pages/AllCities";
+import City from "./pages/City";
+
 
 export default function Router() {
     return (
         <>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Index}/>
-                    <Route path='/login' component={Login}/>
-                    <Route path='/newuser' component={Register}/>
-                    <Route path='/newguide' component={GuideRegister}/>
-                    <Route path='/all_cities' component={AllCities}/>
+                    <Route exact path='/' component={Index} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/newuser' component={Register} />
+                    <Route path='/newguide' component={GuideRegister} />
+                    <Route path='/allCities' component={AllCities} />
+                    <Route path='/city'component={City} />
 
                     {/*<Route path='/dashboard' component={Dashboard}/>
                     <Route path='/record/:user' component={Record}/>*/}
