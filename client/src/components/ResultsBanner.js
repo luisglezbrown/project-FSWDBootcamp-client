bb7d006d65dc192236829338eeeae36b7e9885b6
luisglezbrown <lfernanglez@gmail.com>
2021-05-29 14:08:49 +0200

    Completo componente ResultsBanner

--- client/src/components/ResultsBanner.js
+++ client/src/components/ResultsBanner.js
@@ -1,8 +1,9 @@
 import './style/ResultsBanner.css'
 
-export default function ResultsBanner({ header, text}) {
+export default function ResultsBanner({ header, text, style}) {
+
     return (
-        <div className="banner-container">
+        <div className="banner-container" style={style}>
             <div className="banner-text">
                 <h1>{header}</h1>
                 <p> {text} </p>
