7843e24e7c219c3242d6f0c7ce14815b24aa674f
luisglezbrown <lfernanglez@gmail.com>
2021-05-29 14:08:20 +0200

    Creo componente TourCard

---
+++ client/src/components/TourCard.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import './style/TourCard.css'
+
+export default function TourCard({ tour }) {
+    const IMG_URL = `/images/cities/card-15.png`;
+    const INLINE_STYLE = {backgroundImage: `linear-gradient(270deg, #fafafa 0%, rgba(255,204,0,0) 10%), url(${IMG_URL})`};
+
+    return (
+        <div className="tour-container">
+            <div className="tour-image" style={INLINE_STYLE}>    
+            {tour.categoryLabels.map(category => <span className='category'>#{category}</span>)}
+            </div>
+            
+            <div className="tour-text">
+                <h1>{tour.name}</h1>
+                <span>⏱ {tour.duration} horas</span>
+                <Link to={`/guide/${tour.guideId}`} className='guide-btn'>🗣 {tour.guideName}</Link>  
+                <p>{tour.highlight}</p>
+                <Link to='/newguide' className='book-btn'>reservar</Link>
+            </div>
+        </div>
+    )
+}
