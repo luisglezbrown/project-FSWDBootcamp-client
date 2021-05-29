0f150973d96fe55f1ed48a6d9215051f05d45df8
luisglezbrown <lfernanglez@gmail.com>
2021-05-29 14:09:30 +0200

    refactorizo código

--- client/src/components/CityCard.js
+++ client/src/components/CityCard.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 import './style/CityCard.css'
 
-export default function TopCityCard({ city }) {
+export default function CityCard({ city }) {
     const IMG_URL = "/images/cities/card-";
 
     return (
-        <Link to={`/city?id=${city.id}`} className='city-card'>
+        <Link to={`/city/${city.id}`} className='city-card'>
             <img src={`${IMG_URL}${city.id}.png`} alt={`Vista de ${city.name}`} className="city-card-img" />
             <h3 className="city-card-name">{city.name}</h3>
         </Link>
