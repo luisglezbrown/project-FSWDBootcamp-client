5281e1eb1fe952ad1f476c15e1c12e9170dde853
luisglezbrown <lfernanglez@gmail.com>
2021-05-30 18:06:04 +0200

    arreglo el link una vez creado el componente Guide

--- client/src/components/LastGuideCard.js
+++ client/src/components/LastGuideCard.js
@@ -6,7 +6,7 @@ export default function LastGuideCard({ guide }) {
     const IMG_URL = "/images/guides/guide";
 
     return (
-        <Link to={`/guide?id=${guide.id}`} className='guide-card'>
+        <Link to={`/guide/${guide.id}`} className='guide-card'>
             <img src={`${IMG_URL}${guide.id}.jpg`} alt={`Vista de ${guide.name}`} className="guide-card-img" />
             <h3 className="guide-card-name">{guide.name}</h3>
             <p className="guide-card-city">Ofrece tours en {guide.city}</p>
