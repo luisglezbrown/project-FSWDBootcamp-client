6afb8150b400391d0e5933f0da0efbd2ab2ae71a
luisglezbrown <lfernanglez@gmail.com>
2021-06-04 08:54:51 +0200

    Retoco código para poder enviar style como props

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -1,8 +1,8 @@
 import './style/AnswerCard.css'
 
-export default function AnswerCard({ question, answer, imgpath}) {
+export default function AnswerCard({ question, answer, imgpath, style}) {
     return (
-        <div className="answer-card">
+        <div className="answer-card" style={style}>
             <img src={imgpath} alt={question} />
             <h1>{question}</h1>
             <p>{answer}</p>
