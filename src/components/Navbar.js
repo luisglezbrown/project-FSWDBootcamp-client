25c8e6bd15700e58102e782dbde25554178e8d5b
luisglezbrown <lfernanglez@gmail.com>
2021-06-27 20:41:10 +0200

    Modifico componente para que muestre distinto contenido si está loggeado o no

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,25 @@ import logo2 from '../images/logo2.png'
 
 export default function Navbar() {
 
+    let userLogged = false;
+    /* TODO: ¿Cómo capturo si el usuario está logueado? */
+
+
     return (
         <nav>
             <Link to='/'><img src={logo2} alt="logo" className="logo"/></Link>
-            <div className='button-container'>
-                <Link to='/newuser' className="btn register"> registro</Link>
-                <Link to='/login' className="btn login"> acceso</Link>
-            </div>
+
+            {
+                userLogged
+                ?   <div className='button-container'>
+                        <Link to='/logout' className="btn register"> cerrar sesión</Link>
+                        <Link to='/my-account' className="btn login"> mi cuenta</Link>
+                    </div> 
+                :   <div className='button-container'>
+                        <Link to='/newuser' className="btn register"> registro</Link>
+                        <Link to='/login' className="btn login"> acceso</Link>
+                    </div>            
+            }
         </nav>
     )
 }
