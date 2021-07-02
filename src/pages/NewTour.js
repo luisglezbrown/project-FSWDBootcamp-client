ed27407c5dbcc8ef9f8570c81ad08218857d70f3
luisglezbrown <lfernanglez@gmail.com>
2021-07-02 10:16:24 +0200

    Creo la página "NewTour"

---
+++ src/pages/NewTour.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import NewTourForm from "../components/NewTourForm";
+
+export default function NewTour() {
+    return (
+        <>
+            <Navbar />
+            <NewTourForm />
+        </>
+    )
+}
