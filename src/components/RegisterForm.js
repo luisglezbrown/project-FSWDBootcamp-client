9069e9a9993f8df16ffd5bd9493b558553eee51c
luisglezbrown <lfernanglez@gmail.com>
2021-07-05 00:07:34 +0200

    Añado el fetch para el formulario de registro

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -4,13 +4,24 @@ import './style/RegisterForm.css'
 
 export default function RegisterForm() {
 
-    const initialFormState = {email: "", password: "", name: "", surname: "", phone: "", role: "user"};
+    const initialFormState = {email: "", password: "", name: "", lastname: "", phone: ""};
     const [form, handleInputChange] = useForm(initialFormState); // Custom Hook
 
-    const handleSubmit = e => {
+
+    async function handleSubmit(e){
         e.preventDefault();
-        //TODO: Introducir la lógica del formulario.
-    };
+
+        const options = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(form)
+        }
+
+        const response = await fetch("http://127.0.0.1:8000/api/newuser", options);
+        const data = await response;
+        console.log(data);
+    }
+    
 
     return (
         <section className="register-container">
@@ -20,18 +31,18 @@ export default function RegisterForm() {
             <form onSubmit={handleSubmit} className="register-form-container">
                 <fieldset> 
                     <legend>Tus datos de acceso:</legend> 
-                    <div class="form-group">
-                        <label class="form-label">Email</label>
+                    <div className="form-group">
+                        <label className="form-label">Email</label>
                         <input onChange={handleInputChange} value={form.email} name="email" type="email" placeholder="usuario@tuemail.com" className="form-control" required/>
                     </div>
                     
-                    <div class="form-group">
-                        <label class="form-label">Contraseña</label>
+                    <div className="form-group">
+                        <label className="form-label">Contraseña</label>
                         <input onChange={handleInputChange} value={form.password} name="password" type="password" placeholder="******" className="form-control" required/>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Repite la contraseña</label>
+                    <div className="form-group">
+                        <label className="form-label">Repite la contraseña</label>
                         <input onChange={handleInputChange} value={form.password} name="password" type="password" placeholder="******" className="form-control" required/>
                     </div>
                 {/* TODO: ¿Cómo gestionar la validación de contraseñas? */}
@@ -39,18 +50,18 @@ export default function RegisterForm() {
 
                 <fieldset> 
                     <legend>Tus datos personales:</legend> 
-                    <div class="form-group">
-                        <label class="form-label">Nombre</label>
+                    <div className="form-group">
+                        <label className="form-label">Nombre</label>
                         <input onChange={handleInputChange} value={form.name} name="name" type="text" placeholder="Nombre" className="form-control" required/>
                     </div>
                     
-                    <div class="form-group">
-                        <label class="form-label">Apellidos</label>
-                        <input onChange={handleInputChange} value={form.surname} name="surname" type="text" placeholder="Apellidos" className="form-control" required/>
+                    <div className="form-group">
+                        <label className="form-label">Apellidos</label>
+                        <input onChange={handleInputChange} value={form.lastname} name="lastname" type="text" placeholder="Apellidos" className="form-control" required/>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Teléfono</label>
+                    <div className="form-group">
+                        <label className="form-label">Teléfono</label>
                         <input onChange={handleInputChange} value={form.phone} name="phone" type="tel" placeholder="600600600" className="form-control" required/>
                     </div>
                 </fieldset>
@@ -58,7 +69,6 @@ export default function RegisterForm() {
                 <input type="submit" value="Crear cuenta" className="submit-btn"/>
 
             </form>
-
         </section>
     )
 }
