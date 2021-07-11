b0e1dee8db286deeac5bc8d6d65d37012f7465ab
luisglezbrown <lfernanglez@gmail.com>
2021-07-11 13:34:41 +0200

    Añado redirect según isAuthenticated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,14 @@
+import { Redirect } from "react-router";
+import { useAuthContext } from "../context/AuthContext";
+
 import Navbar from '../components/Navbar';
 import LoginForm from '../components/LoginForm';
 
 export default function Login() {
-    return (
+
+    const {isAuthenticated} = useAuthContext();
+
+    return isAuthenticated ? <Redirect to="/myaccount" /> : (
         <>
             <Navbar />
             <LoginForm />
