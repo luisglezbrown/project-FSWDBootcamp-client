b97d5eaf89638a3359c0a931fb957033cbc64d80
luisglezbrown <lfernanglez@gmail.com>
2021-07-11 13:30:08 +0200

    Añado el paquete Autocomplete y el useEffect que carga la lista de ciudades

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import './style/HeroImage.css';
 
 export default function HeroImage() {
+
+    const [cities, setCities] = useState([])
+    const [cityTarget, setCityTarget] = useState('')
+
+
+    const API_CITIES = "http://127.0.0.1:8000/api/allcities";
+    useEffect(() => {
+        fetch(API_CITIES)
+        .then(response => response.json())
+        .then(data => setCities(data.results))
+    }, [])
+
+    console.log('cityTarget:'+cityTarget)
+
+    let results = cities.filter(city => city.name === cityTarget);
+    let cityId = (results.length > 0) ? results[0].id : null;
+
+    console.log('cityId:'+cityId)
+
     return (
         <div className='hero-container'>
             <div className="hero-card">
@@ -10,8 +35,18 @@ export default function HeroImage() {
                 </div>
                 <div className='hero-input'>
                     <h1>ME VOY A...</h1>
-                    <input type='text' placeholder='ej: París, Madrid, New York...'/>
-                    <button>¡BUSCA!</button>
+                    {/* <input type='text' placeholder='ej: París, Madrid, New York...'/> */}
+                    <Autocomplete
+                    onChange={e => setCityTarget(e.target.innerText)}
+                    id="free-solo-demo"
+                    freeSolo
+                    options={cities.map((city) => city.name)}
+                    renderInput={(params) => (
+                    <TextField {...params} label="Ej: París, Tokio..." margin="normal" variant="outlined" />
+                    )}
+                />
+                <Link to={`/city/${cityId}`} className="btn login">¡BUSCA!</Link>
+                {/* <button onClick={}>¡BUSCA!</button> */}
                 </div>
             </div>
         </div>
