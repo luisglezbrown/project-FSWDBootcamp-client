a0e5331a72d19f39f9fbe5281fa28c1a0d80f38f
luisglezbrown <lfernanglez@gmail.com>
2021-07-11 13:27:21 +0200

    Actualizo etiquetas tras crear el endpoint

--- src/components/GuideDetails.js
+++ src/components/GuideDetails.js
@@ -2,18 +2,18 @@ import './style/GuideDetails.css'
 
 export default function GuideDetails({ guide }) {
 
-    const IMG_URL = "/images/guides/guide";
+    const IMG_URL = "/images/guides/";
 
     return (
         <section className='guide-details-container'>
             <article className='guide-details-text'>
                 <h1>¡Hola, soy {guide.name}!</h1>
-                <span className='city-label'>🗺 {guide.cityName}</span>
+                {/* <span className='city-label'>🗺 {guide.cityName}</span> */}
                 <p>"{guide.shortDesc}"</p>
                 <p className='about-label'>Sobre mí:</p>
-                <p>{guide.longDesc}</p>
+                <p>{guide.description}</p>
             </article>
-            <img src={`${IMG_URL}${guide.id}.jpg`} className='guide-image' alt={guide.name}/>
+            <img src={`${IMG_URL}${guide.imgpath}`} className='guide-image' alt={guide.name}/>
         </section>
     )
 }
--- src/components/LastGuideCard.js
+++ src/components/LastGuideCard.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import './style/LastGuideCard.css'
 
 export default function LastGuideCard({ guide }) {
-    const IMG_URL = "/images/guides/guide";
+    const IMG_URL = "/images/guides/";
 
     return (
         <Link to={`/guide/${guide.id}`} className='guide-card'>
-            <img src={`${IMG_URL}${guide.id}.jpg`} alt={`Vista de ${guide.name}`} className="guide-card-img" />
+            <img src={`${IMG_URL}${guide.imgpath}`} alt={`Vista de ${guide.name}`} className="guide-card-img" />
             <h3 className="guide-card-name">{guide.name}</h3>
-            <p className="guide-card-city">Ofrece tours en {guide.city}</p>
-            <p className="guide-card-about">" {guide.about} "</p>
+            {/* <p className="guide-card-city">Ofrece tours en {guide.city}</p> */}
+            <p className="guide-card-about">" {guide.shortDesc} "</p>
         </Link>
     )
 }
