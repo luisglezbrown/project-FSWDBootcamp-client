11c3b184cb4dece5039526cac4eb835e75289320
luisglezbrown <lfernanglez@gmail.com>
2021-07-11 15:05:31 +0200

    Modifico keys del json para hacerlos más entendibles y muestro días disponibles

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -4,28 +4,34 @@ import { useForm } from '../hooks/useForm';
 import './style/BookingForm.css'
 
 
-export default function Form({booking, setBooking}){
+export default function Form({booking, setBooking, tour}){
 
     let {id} = useParams();
 
-    const initialFormState = {tourId: id, nPax: "", date: ""};
+    const initialFormState = {tour: id, pax: "", date: ""};
     const [form, handleInputChange] = useForm(initialFormState); // Custom Hook
 
     const handleSubmit = e => {
         e.preventDefault();
         setBooking(form)
     };
+    // console.log("tour: " + tour);
+    // console.log(tour);
+    console.log("form: " + form);
+    console.log(form);
+
 
     return(
         <form onSubmit={handleSubmit} className="form-container">
             <div class="form-group">
                 <label class="form-label">¿Qué día?</label>
+                <small>Tour disponible: {tour?.weekDays?.map(day => {if (day==="1") {return "lunes "} else if (day==="2") {return "martes "} else if (day==="3") {return "miércoles "} else if(day==="4") {return "jueves "} else if (day==="5") {return "viernes "} else if (day==="6") {return "sábados "} else if (day==="0") {return "domingos "} return "."})}</small>
                 <input onChange={handleInputChange} value={form.date} name="date" type="date" placeholder="¿Qué día?" className="form-control" required/>
 			</div>
             
             <div class="form-group">
                 <label class="form-label">¿Cuántos sois?</label>
-                <input onChange={handleInputChange} value={form.nPax} name="nPax" type="number" placeholder="Indica el total de personas" className="form-control" required/>
+                <input onChange={handleInputChange} value={form.pax} name="pax" type="number" placeholder="Indica el total de personas" className="form-control" required/>
 			</div>
 
             <input type="submit" value="RESERVAR" className="submit-btn"/>
