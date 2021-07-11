19bfa927a3156d5bffe7e82d70335a6ccd9c6cf7
luisglezbrown <lfernanglez@gmail.com>
2021-07-11 13:26:29 +0200

    Añado key al map

--- src/components/AdvantageCard.js
+++ src/components/AdvantageCard.js
@@ -1,8 +1,8 @@
 import './style/AdvantageCard.css'
 
-export default function AdvantageCard({ title, text, imgpath, style }) {
+export default function AdvantageCard({ title, text, imgpath, style, index }) {
     return (
-        <div className="advantage-card" style={style}>
+        <div className="advantage-card" style={style} key={index}>
             <img src={imgpath} alt={title} />
             <div className="text-container">
                 <h1>{title}</h1>
