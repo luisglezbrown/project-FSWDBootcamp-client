dfcde8061900c79023003599488b7291e585f6db
luisglezbrown <lfernanglez@gmail.com>
2021-07-12 20:12:03 +0200

    Aislo el formulario en un componente y añado lógica a la redirección

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,4 @@
-import { /* Redirect, */ useHistory } from "react-router";
-import { Link } from 'react-router-dom';
+import { /* Redirect, */ useHistory, useLocation } from "react-router";
 import { useForm } from '../hooks/useForm';
 import { useAuthContext } from "../context/AuthContext";
 import { LOGIN_URL } from '../config/config';
@@ -9,7 +8,8 @@ import './style/LoginForm.css'
 export default function LoginForm() {
     const {signIn} = useAuthContext();
     const history = useHistory();
-
+    let location = useLocation()
+    console.log(location);
     const initialFormState = {username: "", password: ""};
     const [form, handleInputChange] = useForm(initialFormState); // Custom Hook
 
@@ -29,7 +29,7 @@ export default function LoginForm() {
 
         if(response.status >= 200 && response.status < 300) {
             signIn(data.token, data.user);
-            history.push("/myaccount")
+            location.pathname === "/login" && history.push("/myaccount")
         } else {
             alert("Login incorrecto");
         }
@@ -37,30 +37,17 @@ export default function LoginForm() {
     
 
     return (
-        <section className="login-container">
-            <div className="login-left">
-                <h1>¡Nos encanta volver a verte!</h1>
-                <p>Introduce tu email y contraseña para acceder al área privada</p>
-                <Link to='/newuser' className="secondary-action">¿No estás registrado? Crea una cuenta</Link>
-
-            </div>
-
-            <div className="login-right">
-                <form onSubmit={handleSubmit} className="form-container">
-                    <div class="form-group">
-                        <label class="form-label">Email</label>
-                        <input onChange={handleInputChange} value={form.username} name="username" type="email" placeholder="usuario@tuemail.com" className="form-control" required/>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label class="form-label">Contraseña</label>
-                        <input onChange={handleInputChange} value={form.password} name="password" type="password" placeholder="******" className="form-control" required/>
-                    </div>
-
-                    <input type="submit" value="Iniciar sesión" className="submit-btn"/>
-
-                </form>
-            </div>
-        </section>
+            <form onSubmit={handleSubmit} className="form-container">
+                <div class="form-group">
+                    <label class="form-label">Email</label>
+                    <input onChange={handleInputChange} value={form.username} name="username" type="email" placeholder="usuario@tuemail.com" className="form-control" required/>
+                </div>
+                
+                <div class="form-group">
+                    <label class="form-label">Contraseña</label>
+                    <input onChange={handleInputChange} value={form.password} name="password" type="password" placeholder="******" className="form-control" required/>
+                </div>
+                <input type="submit" value="Iniciar sesión" className="submit-btn"/>
+            </form>
     )
 }
