e53e6bca6d32b93180b366a559010e271c0af50c
luisglezbrown <lfernanglez@gmail.com>
2021-07-12 20:14:25 +0200

    Introduzco el layout y los textos para aislar el LoginForm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import { Redirect } from "react-router";
 import { useAuthContext } from "../context/AuthContext";
+import { Link } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
 import LoginForm from '../components/LoginForm';
@@ -11,7 +12,16 @@ export default function Login() {
     return isAuthenticated ? <Redirect to="/myaccount" /> : (
         <>
             <Navbar />
-            <LoginForm />
+            <section className="login-container">
+                <div className="login-left">
+                    <h1>¡Nos encanta volver a verte!</h1>
+                    <p>Introduce tu email y contraseña para acceder al área privada</p>
+                    <Link to='/newuser' className="secondary-action">¿No estás registrado? Crea una cuenta</Link>
+                </div>
+                <div className="login-right">
+                    <LoginForm />
+                </div>
+            </section>   
         </>
     )
 }
