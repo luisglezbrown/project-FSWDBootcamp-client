a9fec1ecc1ea1c9fda45e389644f2b925678b0b6
luisglezbrown <lfernanglez@gmail.com>
2021-07-16 17:42:17 +0200

    Modifico textos

--- src/pages/BookingCreated.js
+++ src/pages/BookingCreated.js
@@ -11,7 +11,8 @@ export default function BookingCreated() {
             <section className='h-90vh flex' style={{background: `linear-gradient(0deg, #ffcc00 0%, rgba(255,204,0,0) 55%)`}}>
                 <div className='my-3 center'>
                     <h1 className='header grey-shadow'>¡Reserva realizada correctamente!</h1>
-                    <p className='text'>Estos son los detalles de tu reserva</p>
+                    <p className='text'>Tu localz te espera con muuuuuchas historias que contarte.</p>
+                    <p className='text'> Si por cualquier motivo no puedes realizar el free-tour, puedes cancelar la reserva desde tu cuenta. ¡Tu localz agradecerá el gesto!</p>
                     <div className='button-container my-3'>
                         <Link to='/allCities' className="btn login">¡Explora más tours!</Link>
                     </div>
