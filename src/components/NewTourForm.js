3cdadcc132c459e05cfe8aba05121c3971cfd301
luisglezbrown <lfernanglez@gmail.com>
2021-07-16 17:45:37 +0200

    Reemplazo class por className y for por htmlFor para evitar warnings en consola

--- src/components/NewTourForm.js
+++ src/components/NewTourForm.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useForm } from '../hooks/useForm';
+import { useAuthContext } from "../context/AuthContext";
+import jwt_decode from "jwt-decode";
 import { GET_CITIES_ALL, GET_CATEGORY_ALL, POST_TOUR_NEW, POST_TOUR_IMG } from '../config/config';
 
 import './style/RegisterForm.css'
 
 export default function NewTourForm() {
+    const {getToken} = useAuthContext();
     const history = useHistory();
 
     const [cities, setCities] = useState([]);
@@ -33,15 +36,19 @@ export default function NewTourForm() {
         {id: 0, name: "Domingo"}
     ];
 
+    let token = getToken();
+    let tokenDecoded = jwt_decode(token);
+
     //TODO: Gestionar el userId, debe pillarlo del token
-    const initialFormState = {userId: "1501", cityId: "", title: "", duration: Number(), highlight: "", startingTime: "", meetingPoint: "", description: ""};
+    const initialFormState = {userId: tokenDecoded.user.id, cityId: "", title: "", duration: Number(), highlight: "", startingTime: "", meetingPoint: "", description: ""};
     const [form, 
         handleInputChange, 
         handleCatCheckboxChange, 
         catCheckedState, 
         handleDaysCheckboxChange, 
         daysCheckedState] = useForm(initialFormState, categoriesList, daysList);
-    // console.log(form);
+    console.log('form: ')
+    console.log(form);
 
 
     /* Este bloque gestiona los cambios en la carga de imagen */
@@ -94,68 +101,68 @@ export default function NewTourForm() {
                     <legend>Tu nuevo tour:</legend> 
                     
                     <div className="form-group">
-                        <label for="city_id" className="form-label">Ciudad</label>
+                        <label htmlFor="city_id" className="form-label">Ciudad</label>
                         <select required onChange={handleInputChange} value={form.cityId} name="cityId">
                             <option value="" disabled defaultValue>Selecciona la ciudad</option>
                             {cities.map(city => <option value={city.id} key={city.id}>{city.name}</option>)}
                         </select>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Nombre</label>
+                    <div className="form-group">
+                        <label className="form-label">Nombre</label>
                         <small>Ponle nombre a la criatura, trata de ser original.</small>
                         <input onChange={handleInputChange} value={form.title} name="title" type="text" placeholder="Por ejemplo: 'Leyendas del Barrio de las letras'" required/>
                     </div>
                     
-                    <div class="form-group">
-                        <label class="form-label">Resumen</label>
+                    <div className="form-group">
+                        <label className="form-label">Resumen</label>
                         <small>¡Capta la atención de los viajeros con menos 250 caracteres que definan tu free-tour!</small>
                         <input onChange={handleInputChange} value={form.highlight} name="highlight" type="text" placeholder="Un ejemplo sería 'Un paseo inolvidable por Cartagena.'" required/>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Descripción</label>
+                    <div className="form-group">
+                        <label className="form-label">Descripción</label>
                         <small>Describe todo lo que verán durante el tiempo que compartirás con los viajeros. ¡No olvides incluir cualquier recomendación importante!</small>
                         <textarea onChange={handleInputChange} value={form.description} name="description" type="text" placeholder="No hay límite de caracteres, tu teclado debe echar humo..." required/>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Hora de inicio</label>
+                    <div className="form-group">
+                        <label className="form-label">Hora de inicio</label>
                         <small>¿A qué hora comienza el tour?</small>
                         <input onChange={handleInputChange} value={form.startingTime} name="startingTime" type="time" required/>
                     </div>
 
-                    <div class="form-group">
-                        <label class="form-label">Duración</label>
+                    <div className="form-group">
+                        <label className="form-label">Duración</label>
                         <small style={{display: 'inline'}}>Aproximadamente, mi tour durará unas </small>
                         <input onChange={handleInputChange} value={form.duration} name="duration" type="number" step="0.25" min="0" style={{width: '6rem', display: 'inline'}} placeholder="Si tu free-tour dura dos horas y media, aquí sería '2.5'" required/>
                         <small style={{display: 'inline', margin: 0}}> horas.</small>
                     </div>
 
                     <div className="form-group">
-                        <label for="availableWeekDays" className="form-label">Días de celebración</label>
+                        <label htmlFor="availableWeekDays" className="form-label">Días de celebración</label>
                         <small>Selecciona qué día/s se realiza el tour.</small>
                     </div>
                     <div id="availableWeekDays">
-                        {daysList.map((day, index )=><small className="check"> <input type="checkbox" name="weekDays" value={day.id} checked={daysCheckedState[index]} onChange={() => handleDaysCheckboxChange(index)}/>{day.name}</small>)}
+                        {daysList.map((day, index )=><small className="check" key={day.id}> <input type="checkbox" name="weekDays" value={day.id} checked={daysCheckedState[index]} onChange={() => handleDaysCheckboxChange(index)} key={day.id}/>{day.name}</small>)}
                     </div>
                     
-                    <div class="form-group">
-                        <label class="form-label">Punto de encuentro</label>
+                    <div className="form-group">
+                        <label className="form-label">Punto de encuentro</label>
                         <small>¿Dónde te reunirás con ellos? ¿Cómo te reconocerán?</small>
                         <textarea onChange={handleInputChange} value={form.meetingPoint} name="meetingPoint" type="text" placeholder="Place Charles de Gaulle, justo en la esquina frente al McDonald's. Me reconoceréis por el paraguas verde :)" required className="meeting-point"/>
                     </div>
 
                     <div className="form-group">
-                        <label for="categories" className="form-label">Categorías</label>
+                        <label htmlFor="categories" className="form-label">Categorías</label>
                         <small>Selecciona aquellas categorías para tu tour.</small>
                     </div>
                     <div id="categories">
-                        {categoriesList.map((category, index)=> <small className="check"><input type="checkbox" name="categories" value={category.id} checked={catCheckedState[index]} onChange={() => handleCatCheckboxChange(index)}  /> {category.tag}</small>)}
+                        {categoriesList.map((category, index)=> <small className="check" key={category.id} ><input type="checkbox" name="categories" value={category.id} checked={catCheckedState[index]} onChange={() => handleCatCheckboxChange(index)} key={category.id} /> {category.tag}</small>)}
                     </div>
                     
                     <div className="form-group">
-                        <label for="imgpath" className="form-label">Foto</label>
+                        <label htmlFor="imgpath" className="form-label">Foto</label>
                         <small>Sube una foto de algo representativo o de un grupo de viajeros. Los tours con foto son más llamativos y tienen hasta un 70% más de reservas.</small>
                         <input onChange={handleImageUpload} name="imgpath" type="file" id="imgpath" accept="png jpg jpeg"/>
                     </div>
