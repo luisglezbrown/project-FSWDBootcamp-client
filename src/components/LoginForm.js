2f1b05d766cb408abc167a00fe244f0a085026ff
luisglezbrown <lfernanglez@gmail.com>
2021-07-16 17:38:32 +0200

    Redirecciono según procedencia al hacer login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,17 +2,20 @@ import { /* Redirect, */ useHistory, useLocation } from "react-router";
 import { useForm } from '../hooks/useForm';
 import { useAuthContext } from "../context/AuthContext";
 import { LOGIN_URL } from '../config/config';
+import { Link } from 'react-router-dom';
+import useQuery from "../hooks/useQuery";
+import jwt_decode from "jwt-decode";
 
 import './style/LoginForm.css'
 
 export default function LoginForm() {
     const {signIn} = useAuthContext();
     const history = useHistory();
-    let location = useLocation()
-    console.log(location);
+    let currentLocation = useLocation().pathname;
+    let redirectQuery = useQuery("to");
+
     const initialFormState = {username: "", password: ""};
     const [form, handleInputChange] = useForm(initialFormState); // Custom Hook
-
 
     async function handleSubmit(e){
         e.preventDefault();
@@ -25,11 +28,13 @@ export default function LoginForm() {
 
         const response = await fetch(LOGIN_URL, options);
         const data = await response.json();
-        console.log(data);
+        
+        // const token = data?.token;
+        // const user = jwt_decode(data?.token);
 
         if(response.status >= 200 && response.status < 300) {
-            signIn(data.token, data.user);
-            location.pathname === "/login" && history.push("/myaccount")
+            signIn(data?.token, jwt_decode(data?.token));
+            currentLocation === "/login" && (redirectQuery === "newtour" ? history.push("/newtour") : history.push("/myaccount"))
         } else {
             alert("Login incorrecto");
         }
@@ -38,16 +43,17 @@ export default function LoginForm() {
 
     return (
             <form onSubmit={handleSubmit} className="form-container">
-                <div class="form-group">
-                    <label class="form-label">Email</label>
+                <div className="form-group">
+                    <label className="form-label">Email</label>
                     <input onChange={handleInputChange} value={form.username} name="username" type="email" placeholder="usuario@tuemail.com" className="form-control" required/>
                 </div>
                 
-                <div class="form-group">
-                    <label class="form-label">Contraseña</label>
+                <div className="form-group">
+                    <label className="form-label">Contraseña</label>
                     <input onChange={handleInputChange} value={form.password} name="password" type="password" placeholder="******" className="form-control" required/>
                 </div>
                 <input type="submit" value="Iniciar sesión" className="submit-btn"/>
+                {currentLocation !== "/login" && <Link to='/newuser' className="secondary-action">¿No estás registrado? Crea una cuenta</Link>}
             </form>
     )
 }
