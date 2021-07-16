b332fd0a0577599c46a678e8221d7b6ef4667272
luisglezbrown <lfernanglez@gmail.com>
2021-07-16 17:43:59 +0200

    Creo página del área privada

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { GET_CURRENT_USER } from '../config/config';
+
 import Navbar from '../components/Navbar';
+import BookingTable from "../components/BookingsTable";
+import EditDetailsForm from "../components/EditDetailsForm";
 
 export default function MyAccount() {
+    const {getToken} = useAuthContext();
+    const {isGuide} = useAuthContext();
 
-    const {isGuide, isAdmin, loginUser} = useAuthContext();
+    const [currentUserInfo, setCurrentUserInfo] = useState([]);
+
+    const headers = {
+        headers: {"Authorization": `Bearer ${getToken()}`}
+    };
+
+    useEffect(() => {
+        fetch(GET_CURRENT_USER, headers)
+        .then(response => response.json())
+        .then(data => setCurrentUserInfo(data));
+        // eslint-disable-next-line
+    }, []);
+
 
     return (
         <>
             <Navbar />
-            <p>Hola, estás en el área privada una vez logueado</p>
-            {isGuide? <p>Es guía</p> : <p>No es guía</p>}
-            {isAdmin? <p>Es Admin</p> : <p>No es Admin</p>}
+            <h1>¡Hola, <b>{currentUserInfo?.name}</b>!</h1>
+
+            <h2>Tus reservas</h2>
+            <BookingTable />
+
+            {/* La siguiente línea hace que espere a obtener los datos para cargar el formulario */}
+            {currentUserInfo.length !== 0 ? <EditDetailsForm user={currentUserInfo}/> : <></> }
+
+            {isGuide() && <BookingTable />}
+
         </>
     )
 }
