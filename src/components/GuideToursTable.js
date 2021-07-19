6c7aa4d5b37bb2e4e27b07c8687294a275b14ae1
luisglezbrown <lfernanglez@gmail.com>
2021-07-20 01:36:20 +0200

    Creo elemento GuideToursTable

---
+++ src/components/GuideToursTable.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { GET_BOOKINGS_BY_TOUR } from '../config/config';
+import { useAuthContext } from "../context/AuthContext";
+
+import BookingCancel from './actions/BookingCancel';
+
+export default function GuideToursTable({tourId}) {
+    const {getToken} = useAuthContext();
+    const token = getToken();
+
+    const [tourBookings, setTourBookings] = useState([]);
+
+
+    const headers = {
+        headers: {"Authorization": "Bearer " + token}
+    };
+
+    useEffect(() => {
+        fetch(GET_BOOKINGS_BY_TOUR + tourId, headers)
+        .then(response => response.json())
+        .then(data => setTourBookings(data));
+        // eslint-disable-next-line
+    }, []);
+
+    // console.log(tourBookings)
+    
+    const hasBookings = tourBookings?.totalActiveBookings !== 0;
+    const dateFormat = formerDate => {
+        const splitDate = formerDate.split('-');
+    
+        const year = splitDate[0];
+        const month = splitDate[1];
+        const day = splitDate[2].slice(0,2);
+
+        return (day + '-' + month + '-' + year)
+    }
+    
+    return (
+        hasBookings 
+        ?<table>
+            <thead>
+                <tr>
+                    <th colSpan="4">Tus próximas reservas</th>
+                </tr>
+                <tr>
+                    <th>Fecha</th>
+                    <th>Cliente</th>
+                    <th>Pax</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tourBookings?.active?.map(booking => 
+                <tr key={booking.id}>
+                    <td>{dateFormat(booking.date)}</td>
+                    <td>{booking.clientName}</td>
+                    <td>{booking.pax}</td>
+                    <td><BookingCancel bookingId={booking?.id}/></td>
+                </tr>)}
+            </tbody>
+        </table>
+        :<h2>Este tour aún no tiene reservas</h2>
+    )
+}
