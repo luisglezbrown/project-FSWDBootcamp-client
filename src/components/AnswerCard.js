735e71dd3d5b63f40b3b8011be844924525cbf85
luisglezbrown <lfernanglez@gmail.com>
2021-07-20 00:53:22 +0200

    Mejoro estilos del componente Answers

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -5,7 +5,7 @@ export default function AnswerCard({ question, answer, imgpath, style}) {
     return (
         <div className="answer-card" style={style}>
             <img src={STATIC_FOLDER + "answers/" + imgpath} alt={question} />
-            <h1>{question}</h1>
+            <h1 >{question}</h1>
             <p>{answer}</p>
         </div>
     )
