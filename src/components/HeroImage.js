249f0e0be04f7dfb0a2b13f3f9cf6897beeba533
luisglezbrown <lfernanglez@gmail.com>
2021-07-19 23:42:19 +0200

    Refactorizo el heroImage

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -30,24 +30,19 @@ export default function HeroImage() {
     return (
         <div className='hero-container'>
             <div className="hero-card">
-                <div className='hero-info'>
-                    <h1>La vida es short y el world muy grande...</h1>
-                    <h3>Reserva GRATIS el mejor free-tour con guías locales y no pierdas ni un moment</h3>
-                </div>
-                <div className='hero-input'>
-                    <h1>ME VOY A...</h1>
-                    {/* <input type='text' placeholder='ej: París, Madrid, New York...'/> */}
+                <h1 className='hero-h1'>La vida es short y el world muy grande...</h1>
+                <h3 className='hero-h3 text-shadow'>Reserva GRATIS el mejor free-tour con guías locales y no pierdas ni un moment</h3>
+                <div className=' hero-input flex space-between'>
                     <Autocomplete
-                    onChange={e => setCityTarget(e.target.innerText)}
-                    id="free-solo-demo"
-                    freeSolo
-                    options={cities.map((city) => city.name)}
-                    renderInput={(params) => (
-                    <TextField {...params} label="Ej: París, Tokio..." margin="normal" variant="outlined" />
-                    )}
-                />
-                <Link to={`/city/${cityId}`} className="btn login">¡BUSCA!</Link>
-                {/* <button onClick={}>¡BUSCA!</button> */}
+                        className="input-cities"
+                        onChange={e => setCityTarget(e.target.innerText)}
+                        id="free-solo-demo"
+                        freeSolo
+                        options={cities.map((city) => city.name)}
+                        renderInput={(params) => (
+                        <TextField {...params} label="Me voy a..." margin="normal" variant="outlined" />)}
+                    />
+                    <Link to={`/city/${cityId}`} className="btn-yellow">GO!</Link>
                 </div>
             </div>
         </div>
