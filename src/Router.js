3c25902b75b156d9b7e16780caf11377adf70118
luisglezbrown <lfernanglez@gmail.com>
2021-07-21 00:25:21 +0200

    Añado nuevas rutas

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./hooks/ScrollToTop";
 
 import PrivateRoute from "./pages/PrivateRoute";
+import GuideRoute from "./pages/GuideRoute";
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -19,6 +20,7 @@ import TourEdit from "./pages/TourEdit";
 import UserEdit from "./pages/UserEdit";
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
+import Error from "./pages/Error";
 
 
 export default function Router() {
@@ -33,19 +35,19 @@ export default function Router() {
                     <Route path='/newuser' component={Register} />
                     <Route path='/newguide' component={GuideRegister} />
                     <Route path='/allCities' component={AllCities} />
+                    <Route exact path='/city/null' component={Error} />
                     <Route path='/city/:id'component={City} />
                     <Route path='/guide/:id'component={Guide} />
                     <Route path='/tour/:id'component={Tour} />
                     <Route path='/accountcreated' component={AccountCreated} />
                     <Route path='/tourcreated' component={TourCreated} />
                     <Route path='/bookingcreated' component={BookingCreated} />
-                    <Route path='/newTour' component={NewTour} />
                     <Route path='/touredit' component={TourEdit} />
                     <Route path='/useredit' component={UserEdit} />
+                    <GuideRoute path='/newtour'><NewTour /></GuideRoute>
                     <PrivateRoute path='/myaccount'><MyAccount /></PrivateRoute>
 
-                    {/* TODO: <Route path='/dashboard' component={Dashboard}/>*/}
-                    {/*<Route component={Error}/>*/} {/* TODO: Crear un componente de error */}           
+                    <Route component={Error}/>    
                 </Switch>
             <Footer />
             </BrowserRouter>
